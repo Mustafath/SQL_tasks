e5d6768b004018e199be14bc6dc06de39fc1d298
Mustafa Zakhardin <merdion@icloud.com>
2026-02-15 20:54:46 +0500

    Create TASK1.sql

---
+++ TASK1.sql
@@ -0,0 +1,33 @@
+--- Задача с книги "SQL: Быстрое погружение" (SQLite)
+/* Нужно объединить таблицы "tracks" и "albums" через левое полное соединение и отобрать только те записи,
+которые не имеют значения (NULL) под полем "Composer". В конце отсортировать записи по названию песни.*/
+
+
+
+/*
+1) LEFT JOIN "tracks" TABLE WITH "albums" TABLE  
+AND 
+2) SELECT TRACKS WITHOUT VALUE ON "Composer" COLUMN 
+AND 
+3) SORT BY TRACK NAME (ASCENDING)
+*/
+
+SELECT 
+	t.TrackId,
+	t.Composer,
+	t.Name,
+	a.AlbumId,
+	a.Title
+FROM 
+	tracks AS t
+LEFT OUTER JOIN 
+	albums AS a
+ON
+	t.AlbumId = a.AlbumId 
+WHERE 
+	t.Composer IS NULL --- Only tracks without composer
+ORDER BY
+	t.Name ---Sort by track name
+
+
+--- P.S. Ссылка на саму базу данных в README файле.
