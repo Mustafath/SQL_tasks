6df85a4fde86d674e3cfe47d592cbd53e308789d
Mustafa Zakhardin <merdion@icloud.com>
2026-02-23 02:40:43 +0500

    Update TASK1.sql

--- TASK1.sql
+++ TASK1.sql
@@ -1,16 +1,8 @@
 --- Задача с книги "SQL: Быстрое погружение" (SQLite)
+
 /* Нужно объединить таблицы "tracks" и "albums" через левое полное соединение и отобрать только те записи,
 которые не имеют значения (NULL) под полем "Composer". В конце отсортировать записи по названию песни.*/
 
-
-
-/*
-1) LEFT JOIN "tracks" TABLE WITH "albums" TABLE  
-AND 
-2) SELECT TRACKS WITHOUT VALUE ON "Composer" COLUMN 
-AND 
-3) SORT BY TRACK NAME (ASCENDING)
-*/
 
 SELECT 
 	t.TrackId,
